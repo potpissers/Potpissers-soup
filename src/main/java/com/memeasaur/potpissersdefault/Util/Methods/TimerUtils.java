29c369a1e1b92106290aaa09e6ae58077b29e7e7
Cameron <cuckmeron@gmail.com>
2024-08-13 12:10:08 -0400

    Scoreboard timers

--- src/main/java/com/memeasaur/potpissersdefault/Util/Methods/TimerUtils.java
+++ src/main/java/com/memeasaur/potpissersdefault/Util/Methods/TimerUtils.java
@@ -4,6 +4,17 @@ import com.memeasaur.potpissersdefault.Classes.PlayerData;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+import javax.annotation.Nullable;
+
+import static com.memeasaur.potpissersdefault.Util.Constants.ScoreboardConstants.SCOREBOARD_STRING;
+import static com.memeasaur.potpissersdefault.Util.Methods.CombatUtils.doCombatTag;
 
 public class TimerUtils {
     public static void handleTimerCancel(PlayerData data, Player p) {
@@ -23,4 +34,38 @@ public class TimerUtils {
         if (data.isTpaReady) data.isTpaReady = false;
         // Claims end
     }
+    // Scoreboard timers start
+    public static void doOrUpdateScoreboardTimer(@Nullable Player p, @Nullable Scoreboard scoreboard, String team, int[] timer, @Nullable int[] buffer, int score, int cooldown, Plugin plugin) {
+        timer[0] += cooldown;
+        if (timer[0] > cooldown)
+            return;
+        Objective o = scoreboard.getObjective(SCOREBOARD_STRING);
+        Team t = scoreboard.getTeam(team);
+        //if (cooldown > 3600) {}
+        if (timer[0] > 60)
+            t.suffix(Component.text((timer[0] / 60) + "m" + (timer[0] % 60) + "s"));
+        else
+            t.suffix(Component.text(timer[0]));
+        o.getScore(team).setScore(score);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (timer[0] > 0)
+                    timer[0]--;
+                if (buffer != null && buffer[0] > 0)
+                    buffer[0]--;
+                else if (p != null && timer[0] > cooldown)
+                    doCombatTag(p, plugin);
+                if (timer[0] > 60)
+                    t.suffix(Component.text(((timer[0] / 60) + "m" + timer[0] % 60 + "s")));
+                else
+                    t.suffix(Component.text(timer[0]));
+                if (timer[0] == 0) {
+                    o.getScore(team).resetScore();
+                    cancel();
+                }
+            }
+        }.runTaskTimer(plugin, 20L, 20L);
+    }
+    // Scoreboard timers end
 }
